df87213dc7914d80bfc0427bc4dd965998bdc484
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-07-30 16:15:51 +0100

    Feat: add logic and test methods for calculateSumAscii

---
+++ src/main/java/org/northcoders/Kata03.java
@@ -0,0 +1,21 @@
+package org.northcoders;
+
+public class Kata03 {
+    public int calculateSumAscii(String str){
+        if(str.isEmpty()) throw new IllegalArgumentException();
+        // initialize the sum
+        int sum = 0;
+        // iterate the string
+        for (int i = 0; i < str.toCharArray().length; i++) {
+            // convert current char to ascci int
+            // increment sum with current int
+            int asciiValue = (int) str.charAt(i);
+            sum += asciiValue;
+        }
+
+
+
+
+        return sum;
+    }
+}
---
+++ src/test/java/org/northcoders/Kata03Test.java
@@ -0,0 +1,38 @@
+package org.northcoders;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Kata03Test {
+
+    Kata03 kata;
+    @BeforeEach
+    void setUp() {
+        kata = new Kata03();
+    }
+
+    @Nested
+    @DisplayName("Test method calculateSumAscii")
+    class calculateSumAscii {
+
+        @Test
+        void testEmptyInput() {
+            assertThrows(IllegalArgumentException.class, ()->kata.calculateSumAscii(""));
+        }
+
+        @Test
+        void testMultipleString(){
+            assertAll(
+                    ()-> assertEquals(1195, kata.calculateSumAscii("northcoders"), "return ASCII sum of all lower case letters"),
+                    ()-> assertEquals(1163, kata.calculateSumAscii("Northcoders"), "return ASCII sum of mixed capitalized word"),
+                    () -> assertEquals(97, kata.calculateSumAscii("a"), "return ASCII sum of single letter of string "),
+                    () -> assertEquals(113, kata.calculateSumAscii("89"), "return ASCII sum of two digits")
+            );
+        }
+    }
+
+}
